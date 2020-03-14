6416e617fdff99eb6eb2b637aa53a1e9bd64fd52
Ray Deck <ray@raydeck.com>
2020-03-14 18:15:11 -0400

    Simplifying the default wrapper_base to not call for external dependencies

--- templates/wrapper_base.js
+++ templates/wrapper_base.js
@@ -1,12 +1,6 @@
 
-import { {{{aliases}}} } from "{{{inputPath}}}";
-import "source-map-support/register";
+import { {{{aliases}}} } from "{{{handlersPath}}}";
 import { isArray} from "util"
-import { withAuthentication} from "@privilege/backend-model-2"
-import epsagon from "epsagon";
-import { withBatch } from "@privilege/backend-model-2"
-import sourcemap from 'source-map-support'
-sourcemap.install();
 const withWrapper = f => f;
 {{{wrappers}}}
 const getResolverFunc = (func) => {
@@ -17,19 +11,39 @@ const getResolverFunc = (func) => {
           throw `Unhandled function: ${func}`;
       }
 }
+const withBatch = f => async (args, b, cb) => {
+    if (args.__isBatch) return f(args, b, cb);
+    args.map(async (args,b,cb) => {
+          const newArgs = { ...args, __isBatch: true };
+          try {
+            const data = await f(newArgs, b);
+            return { data };
+          } catch (error) {
+            if (!error) console.error("Error was undefined");
+            else {
+              console.error(JSON.stringify(error));
+              if (error.Fault) console.error(JSON.stringify(error.Fault.Error));
+              if (error.fault) console.error(JSON.stringify(error.fault.error));
+            }
+            return {
+              data: null,
+              errorMessage: error
+                ? error.message
+                  ? error.message.toString()
+                  : error
+                : "Error was undefined",
+              errorType: "ERROR"
+            };
+          }
+        });
+      cb(null, results);
+  };
 const appSyncResolver = withWrapper(async (a, b, c) => {
-    if (a.source === "serverless-plugin-warmup") {
-        if(process.env.epsagonToken) epsagon.label("appsync", "warmup");
-        console.log("Warmup invocation")
-        return; 
-    }
-    console.log("Starting appsyncresolver")
-    // console.log(`received a arg of ${JSON.stringify(a, null, 2)}`);
     if (isArray(a)) {
         return withBatch(async (a, b, c) => {
-        const { func } = a;
-        const toRun = getResolverFunc(func);
-        return toRun(a, b, c);
+            const { func } = a;
+            const toRun = getResolverFunc(func);
+            return toRun(a, b, c);
         })(a, b, c);
     } else {
         const { func } = a;
